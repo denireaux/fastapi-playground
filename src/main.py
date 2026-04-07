db0eb2dacdd5f36cce0eaf8d85f4b18d695742a3
D'Angelo L. DeNiro <dangelo.l.deniro@gmail.com>
2026-04-06 23:41:44 -0600

    refactor

--- src/main.py
+++ src/main.py
@@ -1,35 +1,25 @@
-import logging
-from fastapi import FastAPI
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from src.utils import get_logger, database_init, get_db_conn
 import os
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
+log = get_logger()
 app = FastAPI()
-
-# Database connection URL
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/mydatabase")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autobind=engine)
+engine, SessionLocal = database_init()
 
 @app.on_event("startup")
 def startup_event():
-    logger.info("API started. Songs will be queried every 5 seconds.")
+    log.info("API started. Songs will be queried every 5 seconds.")
 
 @app.get("/songs")
-def get_songs():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM songs;"))
-        return [dict(row._mapping) for row in result]
+def get_songs(conn = Depends(get_db_conn), limit: int = 50):
+    # Notice we don't need 'with' here anymore!
+    result = conn.execute(text("SELECT * FROM songs LIMIT :l"), {"l": limit})
+    return [dict(row._mapping) for row in result]
 
-# Heartbeat Query
 @app.get("/heartbeat-query")
-def query_songs_loop():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT title FROM songs ORDER BY RANDOM() LIMIT 1;"))
-        song = result.fetchone()
-        return {"current_vibe": song[0] if song else "No songs found"}
+def query_songs_loop(conn = Depends(get_db_conn)):
+    # Using your random logic from earlier
+    result = conn.execute(text("SELECT title FROM songs ORDER BY RANDOM() LIMIT 1;"))
+    song = result.fetchone()
+    return {"current_vibe": song[0] if song else "No songs found"}
